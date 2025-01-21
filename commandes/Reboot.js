10a92dda7b645d969e97e1640364ab3940778cf5
Mustaffamkm <kimongemustaffa@gmail.com>
2025-01-21 09:12:25 +0300

    Update Reboot.js

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("POPKID-MD bot Restarting ⚽");
+    repondre("MUSTAFFA-MD bot Restarting ⚽");
 
   exec("pm2 restart all");
   

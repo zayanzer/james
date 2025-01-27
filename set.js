c3bf46197c9850d1c4233c7d5127fd7e1f714aef
Popkid <popkidtechs@gmail.com>
2025-01-27 10:38:57 +0300

    ⭕POP KID⭕

--- set.js
+++ set.js
@@ -7,27 +7,58 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Ibrahim Adams",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
-    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'BMW_MD',
-    URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/17c83719a1b40e02971e4.jpg',
-    MODE: process.env.PUBLIC_MODE || "yes",
-    PM_PERMIT: process.env.PM_PERMIT || 'yes',
-    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+module.exports = { session: process.env.SESSION_ID || 'BELTAH-MD;;;',
+    PREFIXE: process.env.PREFIX || "+",
+    CHAT_BOT : process.env.CHAT_BOT|| "non",
+    OWNER_NAME : process.env.OWNER_NAME || "Beltah Tech",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254114141192",
+    ANTICALL: process.env.ANTICALL || "non",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "non",
+    AUTO_BIO: process.env.AUTO_BIO || "non",
+    ANTIDELETEDM: process.env.ANTIDELETEDM|| "non", 
+    ANTIVV: process.env.ANTIVV|| "non", 
+    ADMGROUP: process.env.ADMGROUP || "non", 
+    AUTO_SAVE_CONTACTS: process.env.AUTO_SAVE_CONTACTS || "non", 
+    AUTO_REPLY: process.env.AUTO_REPLY || "non",              
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTOREAD_MESSAGES: process.env.AUTOREAD_MESSAGES || "non",
+    AUTO_REACT: process.env.AUTO_REACTION || "non",
+    ANTILINK :process.env.ANTILINK || "non", 
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F",
+    WEBSITE :process.env.GURL || "https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F",
+    CAPTION : process.env.CAPTION || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ BELTAH-MD",
+    BOT : process.env.BOT_NAME || 'BELTAH-MD',
+    URL : process.env.BOT_MENU_LINKS || '',
+    MODE: process.env.PUBLIC_MODE || "no",
+    TIMEZONE: process.env.TIMEZONE || "Africa/Nairobi", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY || null,
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
     ETAT : process.env.PRESENCE || '',
-    CHATBOT : process.env.PM_CHATBOT || 'no',
+    //GPT : process.env.OPENAI_API_KEY || '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',
     DP : process.env.STARTING_BOT_MESSAGE || "yes",
     ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    CHATBOT : process.env.PM_CHATBOT || 'no',  
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
-   
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {

e5624f2c4d89a3e52adb9b510574172c0de4bf2f
jamestech.ke <logomakingpro21@gmail.com>
2025-02-18 03:15:56 +0300

    Update Reboot.js

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("MUSTAFFA-MD bot Restarting 🕷️");
+    repondre("JAMES-MD bot Restarting 💀");
 
   exec("pm2 restart all");
   
